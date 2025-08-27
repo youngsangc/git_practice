a1b0f0a26878fee20a774166ad444bfef6d127f2
youngsangc <jomediaslack@gmail.com>
2025-08-27 20:10:47 +0900

    add ff

--- main.c
+++ main.c
@@ -5,15 +5,14 @@ void main(){
     int a;
     int b;
 
-<<<<<<< HEAD
     
 
     int d;
     int e;
     
-=======
     int c;
->>>>>>> 5d16c5aeaecfcde9763763e6fcf30a5dbd80831d
+
+    int ff;
 
     return 0;
 
